04632be4593306fa8f940fde222c607695a317a6
Yoann Audouin <yoann.audouin@edf.fr>
2023-03-28 09:02:14 +0200

    Corrections for windows

--- src/NETGENPlugin/NETGENPlugin_NETGEN_3D_Remote.cxx
+++ src/NETGENPlugin/NETGENPlugin_NETGEN_3D_Remote.cxx
@@ -233,13 +233,18 @@ bool NETGENPlugin_NETGEN_3D_Remote::Compute(SMESH_Mesh&         aMesh,
 
   // Temporary folder for run
 #ifdef WIN32
-  fs::path tmp_folder = aMesh.GetTmpFolder() / fs::path("Volume-%%%%-%%%%");
+  // On windows mesh does not have GetTmpFolder
+  fs::path tmp_folder = fs::path("Volume-%%%%-%%%%");
 #else
   fs::path tmp_folder = aMesh.GetTmpFolder() / fs::unique_path(fs::path("Volume-%%%%-%%%%"));
 #endif
   fs::create_directories(tmp_folder);
   // Using MESH2D generated after all triangles where created.
+#ifdef WIN32
+  fs::path mesh_file=fs::path("Mesh2D.med");
+#else
   fs::path mesh_file=aMesh.GetTmpFolder() / fs::path("Mesh2D.med");
+#endif
   fs::path element_orientation_file=tmp_folder / fs::path("element_orientation.dat");
   fs::path new_element_file=tmp_folder / fs::path("new_elements.dat");
   fs::path tmp_mesh_file=tmp_folder / fs::path("tmp_mesh.med");
